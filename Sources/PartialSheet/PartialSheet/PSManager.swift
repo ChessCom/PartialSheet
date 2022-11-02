e319372583311a8f55e071ec31f7472ab01cf8e2
nikitaame <nikitaame@gmail.com>
2022-11-02 10:35:54 -0700

    Update PSManager.swift

--- Sources/PartialSheet/PartialSheet/PSManager.swift
+++ Sources/PartialSheet/PartialSheet/PSManager.swift
@@ -19,14 +19,7 @@ import SwiftUI
 class PSManager: ObservableObject {
     
     /// Published var to present or hide the partial sheet
-    @Published var isPresented: Bool = false {
-        didSet {
-            if !isPresented {
-                content = EmptyView().eraseToAnyView()
-                onDismiss = nil
-            }
-        }
-    }
+    @Published var isPresented: Bool = false
 
     /// The content of the sheet
     @Published private(set) var content: AnyView
