27615c616c985ee14a03637ab4eda04aa22617d5
nikitaame <nikitaame@gmail.com>
2022-11-07 09:39:16 -0800

    Update PSViewModifier.swift

--- Sources/PartialSheet/PartialSheet/PSViewModifier.swift
+++ Sources/PartialSheet/PartialSheet/PSViewModifier.swift
@@ -110,10 +110,12 @@ public struct PartialSheet: ViewModifier {
             // display the sheet content as a normal sheet
                 .iPadOrMac {
                     $0
-                        .sheet(isPresented: $manager.isPresented, onDismiss: {
+//                        .sheet(isPresented: $manager.isPresented, onDismiss: {
+                        .formSheet(isPresented: $manager.isPresented, onDismiss: {
                             self.manager.onDismiss?()
                         }, content: {
                             self.iPadAndMacSheet()
+                                .fixedSize()
                         })
                 }
             // if the device type is an iPhone,
@@ -149,7 +151,6 @@ extension PartialSheet {
                 EmptyView()
             }
             self.manager.content
-            Spacer()
         }.background(self.background)
     }
     
@@ -257,5 +258,91 @@ extension PartialSheet {
             self.dismissKeyboard()
             self.manager.onDismiss?()
         }
+    }
+}
+
+class FormSheetWrapper<Content: View>: UIViewController, UIPopoverPresentationControllerDelegate {
+
+    var content: () -> Content
+    var onDismiss: (() -> Void)?
+
+    private var hostVC: UIHostingController<Content>?
+
+    required init?(coder: NSCoder) { fatalError("") }
+
+    init(content: @escaping () -> Content) {
+        self.content = content
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    func show() {
+        guard hostVC == nil else { return }
+        let vc = UIHostingController(rootView: content())
+
+        vc.view.sizeToFit()
+        vc.preferredContentSize = vc.view.bounds.size
+        vc.preferredContentSize = vc.view.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+
+        vc.modalPresentationStyle = .formSheet
+        vc.presentationController?.delegate = self
+        hostVC = vc
+        self.present(vc, animated: true, completion: nil)
+    }
+
+    func hide() {
+        guard let vc = self.hostVC, !vc.isBeingDismissed else { return }
+        dismiss(animated: true, completion: nil)
+        hostVC = nil
+    }
+
+    func presentationControllerWillDismiss(_ presentationController: UIPresentationController) {
+        hostVC = nil
+        self.onDismiss?()
+    }
+}
+
+struct FormSheet<Content: View>: UIViewControllerRepresentable {
+
+    @Binding var show: Bool
+
+    let onDismiss: (() -> Void)?
+    let content: () -> Content
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<FormSheet<Content>>) -> FormSheetWrapper<Content> {
+
+        let vc = FormSheetWrapper(content: content)
+        vc.onDismiss = {
+            self.show = false
+            self.onDismiss?()
+        }
+        return vc
+    }
+
+    func updateUIViewController(
+        _ uiViewController: FormSheetWrapper<Content>,
+        context: UIViewControllerRepresentableContext<FormSheet<Content>>
+    ) {
+        if show {
+            uiViewController.show()
+        }
+        else {
+            uiViewController.hide()
+        }
+    }
+}
+
+extension View {
+    public func formSheet<Content: View>(
+        isPresented: Binding<Bool>,
+        onDismiss: (() -> Void)?,
+        @ViewBuilder content: @escaping () -> Content
+    ) -> some View {
+        self.background(
+            FormSheet(
+                show: isPresented,
+                onDismiss: onDismiss,
+                content: content
+            )
+        )
     }
 }
