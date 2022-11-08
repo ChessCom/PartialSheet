39f035338a5b19cd8252decafcaab247308dc003
nikitaame <nikitaame@gmail.com>
2022-11-08 09:03:53 -0800

    Update PSSlideAnimation.swift

--- Sources/PartialSheet/PartialSheet/PSSlideAnimation.swift
+++ Sources/PartialSheet/PartialSheet/PSSlideAnimation.swift
@@ -13,7 +13,7 @@ public struct PSSlideAnimation {
 
     /// The default slide in/out animation of the partial sheet
     private(set) var defaultSlideAnimation: Animation = {
-        .easeInOut
+        .easeInOut(duration: 0.35)
 //        .interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0)
     }()
 
